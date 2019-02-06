0c02fbc4e8ee2b9b5c6836255748bfd9d75c7144
Rostislav Mykhajliw <necromant2005@gmail.com>
2019-02-06 10:40:34 +0200

    add magic cmd

--- docker/run.sh
+++ docker/run.sh
@@ -4,9 +4,5 @@ _CONTENT=$(echo $1 | base64 --decode)
 _FILENAME=`echo $2 | base64 --decode`
 _CMD=`echo $3 | base64 --decode`
 
-echo $_CONTENT
-echo $_FILENAME
-echo $_CMD
-
 echo $_CONTENT > $_FILENAME
 echo `$_CMD`
